cb919f33e3aa8ae5b9be4acf05e69559e785aa03
Gercov <51915184+zelendon@users.noreply.github.com>
2021-04-15 21:36:11 +0300

    header

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="orange darken-4">
+    <div class="nav-wrapper container">
+      <a href="#" class="brand-logo">FullNotes</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="sass.html">Notes</a></li>
+        <li><a href="sass.html">To-Do</a></li>
+        <li><a href="badges.html"></a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+  #nav-mobile a {
+    font-size: 18px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import App from "./App.vue"
 import router from "./router";
+import "./registerServiceWorker";
+import 'materialize-css/dist/js/materialize.min.js'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'material-design-icons/iconfont/material-icons.css'
 
 Vue.config.productionTip = false;
 
