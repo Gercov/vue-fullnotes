6acc42f163cc0e941d79cc495b0935c4c649069d
Gercov <51915184+zelendon@users.noreply.github.com>
2021-04-15 22:02:56 +0300

    removed materialize

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,6 @@
 <template>
   <nav class="orange darken-4">
-    <div class="nav-wrapper container">
-      <a href="#" class="brand-logo">FullNotes</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="sass.html">Notes</a></li>
-        <li><a href="sass.html">To-Do</a></li>
-        <li><a href="badges.html"></a></li>
-      </ul>
-    </div>
-  </nav>
+
 </template>
 
 <script>
@@ -18,7 +10,5 @@ export default {
 </script>
 
 <style scoped>
-  #nav-mobile a {
-    font-size: 18px;
-  }
+
 </style>
--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    
-  </div>
+
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
 
 </style>
