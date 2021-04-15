d8d2884273313686d3db0cb060dd7e4ee019ec63
Gercov <51915184+zelendon@users.noreply.github.com>
2021-04-15 22:14:41 +0300

    buefy & header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,15 @@
 <template>
-  <nav class="orange darken-4">
-
+  <div class="container">
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item class="logo"> FullNotes </b-navbar-item>
+      </template>
+      <template #end>
+        <b-navbar-item href="#"> Notes </b-navbar-item>
+        <b-navbar-item href="#"> To-Do </b-navbar-item>
+      </template>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -10,5 +19,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .logo {
+  font-size: 30px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue"
 import router from "./router";
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.use(Buefy)
 
 new Vue({
   router,
